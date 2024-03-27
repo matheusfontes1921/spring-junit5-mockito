91cd2ff876dd716585fb43654c1f6b49e7c3ca55
matheusfontes1921 <matheusfontes1921@gmail.com>
2024-03-27 13:36:18 -0300

    updting

--- src/main/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersController.java
@@ -14,7 +14,7 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UsersController {
 
     UsersService usersService;
