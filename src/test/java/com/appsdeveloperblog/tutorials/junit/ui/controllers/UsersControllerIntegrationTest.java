6e91e987212c6fcd72e6f246ddb147e9383ba582
matheusfontes1921 <matheusfontes1921@gmail.com>
2024-04-01 09:52:14 -0300

    @TestPropertySource. Loading alternative configuration

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,13 +1,20 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-properties = {"server.port=8081", "hostname=192.168.0.2"}) /*this value will override the value in application.properties*/
+import javax.validation.Valid;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(locations = "/application-test.properties", properties = {"server.port=8081"})/*this value will override the value in application.properties*/
 public class UsersControllerIntegrationTest {
+    /*annotation and variable created to verify the current server port value*/
+    @Value("${server.port}")
+    private int serverPort;
     @Test
     void contextLoads() {
-
+        System.out.println(serverPort);
     }
 }
