2527427474f94cfa7fb6c21d8784f2fec36cd7a2
matheusfontes1921 <matheusfontes1921@gmail.com>
2024-04-01 10:37:47 -0300

    TestRestTemplate - Prepare & Perform HTTP Post Request

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,14 +1,21 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.context.TestPropertySource;
 
 import javax.validation.Valid;
+
+import java.util.Arrays;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) /*allow integration tests to run in paralel*/
 //@TestPropertySource(locations = "/application-test.properties", properties = {"server.port=8081"})/*this value will override the value in application.properties*/
@@ -18,6 +25,8 @@ public class UsersControllerIntegrationTest {
     private int serverPort;
     @LocalServerPort
     private int localServerPort;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
     @Test
     void testCreateUser_whenValidDetailsAreProvided_returnsUserDetails() throws JSONException {
         //Arrange
@@ -27,6 +36,22 @@ public class UsersControllerIntegrationTest {
         userDetailsRequestJson.put("email", "gabriel@test.com");
         userDetailsRequestJson.put("password", "123456789");
         userDetailsRequestJson.put("repeatPassword", "123456789");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(userDetailsRequestJson.toString(),headers);
+
+        //Act
+        ResponseEntity<UserRest> createdUserDetailsEntity = testRestTemplate.postForEntity("/users", request, UserRest.class);
+        UserRest createdUserDetails = createdUserDetailsEntity.getBody();
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.OK, createdUserDetailsEntity.getStatusCodeValue());
+        Assertions.assertEquals(userDetailsRequestJson.getString("firstName"), createdUserDetails.getFirstName(),"First name is not equal");
+        Assertions.assertEquals(userDetailsRequestJson.getString("email"), createdUserDetails.getEmail(), "Email is not equal");
+        Assertions.assertFalse(createdUserDetails.getUserId().trim().isEmpty(), "User id should not be empty");
 
     }
 
