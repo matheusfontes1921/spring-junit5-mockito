ea5ad3bb973f062abc65b09e519712f41c226090
matheusfontes1921 <matheusfontes1921@gmail.com>
2024-04-01 10:10:02 -0300

    @SpringBootTest: Random Port Number

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -3,18 +3,22 @@ package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
 
 import javax.validation.Valid;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@TestPropertySource(locations = "/application-test.properties", properties = {"server.port=8081"})/*this value will override the value in application.properties*/
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) /*allow integration tests to run in paralel*/
+//@TestPropertySource(locations = "/application-test.properties", properties = {"server.port=8081"})/*this value will override the value in application.properties*/
 public class UsersControllerIntegrationTest {
     /*annotation and variable created to verify the current server port value*/
     @Value("${server.port}")
     private int serverPort;
+    @LocalServerPort
+    private int localServerPort;
     @Test
     void contextLoads() {
         System.out.println(serverPort);
+        System.out.println(localServerPort);
     }
 }
