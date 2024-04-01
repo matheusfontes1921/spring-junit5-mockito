513eefc5fbd4d649a6d28b05c17c0c59e3ca922f
matheusfontes1921 <matheusfontes1921@gmail.com>
2024-04-01 15:54:07 -0300

    Exercise - Test Find By User Id

--- src/test/java/com/appsdeveloperblog/tutorials/junit/io/UsersRepositoryTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/io/UsersRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.appsdeveloperblog.tutorials.junit.io;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,25 +11,41 @@ import java.util.UUID;
 
 @DataJpaTest
 public class UsersRepositoryTest {
+    UserEntity userEntity;
     @Autowired
     TestEntityManager testEntityManager;
     @Autowired
     UsersRepository usersRepository;
-    @Test
-    void testFindByEmail_whenValidEmailAddressIsProvided_returnsUserEntity(){
-        //Arrange
-        UserEntity userEntity = new UserEntity();
+    @BeforeEach
+    void setUp(){
+        userEntity = new UserEntity();
         userEntity.setFirstName("Gabriel");
         userEntity.setLastName("Fontes");
         userEntity.setEmail("gabriel@test.com");
         userEntity.setUserId(UUID.randomUUID().toString());
         userEntity.setEncryptedPassword("123456789");
         testEntityManager.persistAndFlush(userEntity);
+    }
+
+    @Test
+    void testFindByEmail_whenValidEmailAddressIsProvided_returnsUserEntity(){
+        //Arrange
+
 
         //Act
         UserEntity userPersisted = usersRepository.findByEmail(userEntity.getEmail());
 
         //Assert
         Assertions.assertEquals(userEntity.getEmail(), userPersisted.getEmail(),"The email should be equal");
+    }
+    @Test
+    void testFindByUserId_whenValidUserIdIsProvided_returnsUserEntity() {
+        //Arrange
+
+        //Act
+        UserEntity userPersisted = usersRepository.findByUserId(userEntity.getUserId());
+
+        //Assert
+        Assertions.assertEquals(userEntity.getUserId(), userPersisted.getUserId(), "User ID should be equal");
     }
 }
